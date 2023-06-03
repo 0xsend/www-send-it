0192a553ad86b6761e08650c623c25a7434e99b0
BigBoss <bigboss@sendnodes.io>
2023-06-03 13:48:45 -0500

    center that text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,9 @@ export default function Home() {
       <div className="relative flex flex-col">
         {/* <div className=" before:absolute before:h-[500px] before:w-[780px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[560px] after:w-[540px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[490px]" /> */}
         <div className="w-full md:max-w-md bg-black bg-opacity-50 justify-center">
-          <h1 className="text-4xl sm:text-6xl mb-6 font-semibold">Send</h1>
+          <h1 className="text-4xl sm:text-6xl mb-6 font-semibold text-center">
+            Send
+          </h1>
 
           <div className="flex justify-center gap-4">
             <a href="https://t.co/hr2OkXj0eZ" className="">
